221837fd2b24277042a7c86b5d283e675ed98d0a
taiyoslime <hase.tekitou@gmail.com>
2017-01-01 15:27:34 +0900

    2017

---
+++ 2017.rb
@@ -0,0 +1,30 @@
+eval$s=%w'b="BAhsKwHy//////////////////////////////////////////////////////////////////////////////////////8/AAAAAAAAAAAAAAAAAPj/
+fwAAAAAAAAAAAAAAAADw//8AAAAAAAAAAAAAAAAA4P//AQAAAAAAAAAAAAAAAMD//wMAAAAAAAAAAAAAAACA//8H+P//AP//H+AH+P//AP//D/D//wH+/z/AD/D//wH+/
+x8AAPAD/AB+gB8AAPAD/P8/AADgB/gB/AA/AADgB/j/fwAAwA/wA/gBfgAAwA/w//8A//8f4AfwA/wAAIAf4P//Af7/P8AP4Af4AQAAP8D//wP8AQCAH8AP8AMAAH6A//
+8H+AMAAD+AH+AHAAD8AP//D/AHAAB+AD/ADwAA+AH+/x/g//8D/P9/gB8AAPAD/P8/wP//B/j//wA/AADgB/j/fwAAAAAAAAAAAAAAAADw//8AAAAAAAAAAAAAAAAA4P/
+/AQAAAAAAA                                                                                                             AAAAAAAAMD
+//wMAAAAAA                                                                                                             AAAAAAAAAC
+A//8HAAAAA                                                                                                             AAAAAAAAAA
+AAP///////                                                                                                             //////////
+//////////                                                                                                             //////////
+//////////        /////////////////////        //////////////////Pw=        =";n=M        arshal.load(b.unpack(        "m")[0]);e
+="eval$s=%        w"<<39<<($s*3);o="";j        =-1;0.upto(30*130-28)        {|i|o<        <((n[i]==1)?e[j+=1]:3        2);o<<((i%
+129==128)?                       10:"")        };o[-1         0,6]="        "<<39<                       <".joi        n";puts(o)
+#b="BAhsKw                       Hy////        //////         //////        //////                       //////        //////////
+//////////                       //////        //////         //////        //////                       //////        ////////8/
+AAAAAAAAAA        AAAAAAAPj/fwAAAAAAAAA        AAAAAA         ADw//8        AAAAAA                       AAAAAA        AAAAA4P//A
+QAAAAAAAAA        AAAAAAMD//wMAAAAAAAAA        AAAAAA         CA//8H        +P//AP                       //H+AH        +P//AP//D/
+D//wH+/z/A        D/D//wH                      +/x8AA         PAD/AB        +gB8AA                       PAD/P8        /AADgB/gB/
+AA/AADgB/j        /fwAAwA                      /wA/gB         fgAAwA        /w//8A                       //8f4A        fwA/wAAIAf
+4P//Af7/P8        AP4Af4A                      QAAP8D         //wP8A        QCAH8A                       P8AMAA        H6A//8H+AM
+AAD+AH+AHA        AD8AP//D/AHAAB+AD/ADw        AA+AH+/x/g//8D/P9/gB8        AAPAD/                       P8/wP/        /B/j//wA/A
+ADgB/j/fwA        AAAAAAAAAAAAAAADw//8A        AAAAAAAAAAAAAAAA4P//A        QAAAAA                       AAAAAA        AAAAMD//wM
+AAAAAAAAAA                                                                                                             AAAAACA//8
+HAAAAAAAAA                                                                                                             AAAAAAAAP/
+//////////                                                                                                             //////////
+//////////                                                                                                             //////////
+//////////                                                                                                             //////////
+/////////////////////////Pw==";n=Marshal.load(b.unpack("m")[0]);e="eval$s=%w"<<39<<($s*3);o="";j=-1;0.upto(30*130-28){|i|o<<((n[i
+]==1)?e[j+=1]:32);o<<((i%129==128)?10:"")};o[-10,6]=""<<39<<".join";puts(o)#b="BAhsKwHy//////////////////////////////////////////
+////////////////////////////////////////////8/AAAAAAAAAAAAAAAAAPj/fwAAAAAAAAAAAAAAAADw//8AAAAAAAAAAAAAAAAA4P//AQAAAAAAAAAAAAAAAMD
+//wMAAAAAAAAAAAAAAACA//8H+P//AP//H+AH+P//AP//D/D//wH+/z/AD/D//wH+/x8AAPAD/AB+gB8AAPAD/P8/AADgB/gB/AA/AADgB/j/fwAAwA/wA/gBfg'.join
